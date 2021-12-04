ff40f817b69e40c40849353f2ad6a659e4eedcb0
Kevin Wood <kevin.wood75@gmail.com>
2021-12-04 14:22:21 -0500

    fixed dashboard

---
+++ src/cache_df.py
@@ -0,0 +1,17 @@
+import pyarrow as pa
+import redis
+import yfinance as yf
+
+def cache_df(alias,df):
+    pool = redis.ConnectionPool(host='192.168.2.233', port='6379', db='stocks')
+    cur = redis.Redis(connection_pool=pool)
+    context = pa.default_serialization_context()
+    df_compressed =  context.serialize(df).to_buffer().to_pybytes()
+
+    res = cur.set(alias,df_compressed)
+    if res == True:
+        print('df cached')
+
+
+import_df = yf.download("TSLA","2015-01-01","2021-12-04")
+cache_df("tsla", import_df)
