82eff034fe407e991a079a267780246041bdf0fd
Kevin Wood <kevin.wood75@gmail.com>
2021-12-13 17:27:25 -0500

    test

--- src/stockfi/mybag/forms.py
+++ src/stockfi/mybag/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class StockForm(forms.Form):
-    stock_symbol = forms.CharField(label = "Stock Symbol Search",max_length =240)
+    stock_symbol = forms.CharField(label = "Stock Search",max_length =240)
