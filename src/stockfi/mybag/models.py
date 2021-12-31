1a9089b6eb275a78a12445476a44177cce63f241
Kevin Wood <kevin.wood75@gmail.com>
2021-12-30 23:38:21 -0500

    test

--- src/stockfi/mybag/models.py
+++ src/stockfi/mybag/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class MyBag(models.Model):
+    username = models.CharField(max_length=80)
+    stock_ticker = models.CharField(max_length=20)
+    number_shares = models.IntegerField()
