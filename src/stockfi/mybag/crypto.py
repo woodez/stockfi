ec0b5964a942e148f0bffae12d756cae4d2b2b83
Kevin Wood <kevin.wood75@gmail.com>
2022-01-23 23:38:14 -0500

    test

--- src/stockfi/mybag/crypto.py
+++ src/stockfi/mybag/crypto.py
@@ -32,19 +32,14 @@ class Crypto:
     
    def get_mybtc_table(self):
        crypto_data = self.get_cached_df("BTC-CAD-HIST")
-#       crypto_data["myvalue"] = crypto_data['Close'] * self.mysatoshi
-       my_btc = crypto_data['Close'] * self.mysatoshi
-#       crypto_data['myvalue'] = crypto_data['myvalue'].pct_change() * 100
-       pct_chg = '{:,.2f}'.format(my_btc.pct_change() * 100)
-       crypto_data['myvalue'] = '{:,.2f}%({:,.2f})'.format(pct_chg,my_btc)
+       crypto_data["myvalue"] = crypto_data['Close'] * self.mysatoshi
+       crypto_data['myvalue'] = crypto_data['myvalue'].pct_change() * 100
        crypto_data = crypto_data.tail(10)
        crypto_dict = crypto_data.to_dict()
        btc_dict = {}
        for line in sorted(crypto_dict.get('myvalue').keys(), reverse=True):
            key = str(line).split(" ")[0]
-#           value = '${:,.2f}'.format(portfolio_dict.get('value')[line])
-##           value = '{:,.2f}'.format(crypto_dict.get('myvalue')[line])
-           value = crypto_dict.get('myvalue')[line]
+           value = '{:,.2f}'.format(crypto_dict.get('myvalue')[line])
            tmpdict = { key:value }
            btc_dict.update(tmpdict)
        return btc_dict
