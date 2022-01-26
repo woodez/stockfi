0fa9ed06676f30ea94f7c882f7e8f553ad056353
Kevin Wood <kevin.wood75@gmail.com>
2022-01-26 00:28:14 -0500

    test

--- src/stockfi/mybag/graph.py
+++ src/stockfi/mybag/graph.py
@@ -19,16 +19,13 @@ def get_cached_df(alias):
     pool = redis.ConnectionPool(host='redis01.woodez.net',port='6379', db=0) 
     cur = redis.Redis(connection_pool=pool)
     context = pa.default_serialization_context()
-   ## all_keys = [key.decode("utf-8") for key in cur.keys()]
+    all_keys = [key.decode("utf-8") for key in cur.keys()]
 
-    if alias in all_keys:   
-        result = cur.get(alias)
+#    if alias in all_keys:   
+    result = cur.get(alias)
+    dataframe = pd.DataFrame.from_dict(context.deserialize(result))
 
-        dataframe = pd.DataFrame.from_dict(context.deserialize(result))
-
-        return dataframe
-
-    return None
+    return dataframe
 
 
 def return_graph(symbol,years,gtype):
